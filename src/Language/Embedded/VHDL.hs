78c92cf0aeaa75a26f043fc9a64d8347ad5c57c0
Markus <mararon@chalmers.se>
2015-09-10 19:35:49 +0200

    main module should only export Expr type

--- src/Language/Embedded/VHDL.hs
+++ src/Language/Embedded/VHDL.hs
@@ -1,11 +1,11 @@
 module Language.Embedded.VHDL
   ( Mode(..), Type(..), Kind(..)
+  , Expr
   , std_logic
   , signed,  signed8,  signed16,  signed32,  signed64
   , usigned, usigned8, usigned16, usigned32, usigned64
                                              
   , module Language.Embedded.VHDL.Interface
-  , module Language.Embedded.VHDL.Expression
   , module Language.Embedded.VHDL.Command
   ) where
 
