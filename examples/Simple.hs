46767117c09da6519553c41fb8cf3b1d5e90bd00
Markus <mararon@chalmers.se>
2016-01-13 10:03:18 +0100

    added array example

--- examples/Simple.hs
+++ examples/Simple.hs
@@ -6,6 +6,8 @@ import Language.Embedded.VHDL
 import Control.Monad.Identity
 import Control.Monad.Operational.Higher
 import Data.ALaCarte
+import Data.Int
+import Data.Word
 import Text.PrettyPrint
 
 import Prelude hiding (and)
@@ -32,17 +34,31 @@ type Prog = Program CMD
 testSimple :: Prog ()
 testSimple = do
   i <- newEntity "simple" $
-         do x <- newPort  InOut true :: Prog (Signal Bool)
-            y <- newPort_ In         :: Prog (Signal Bool)
+         do x <- newPort     InOut true :: Prog (Signal Bool)
+            y <- newPort_    In         :: Prog (Signal Word8)
+            z <- newGeneric_ In         :: Prog (Signal Int16)
             return x
   newArchitecture "simple" "behavioural" $
     newProcess [toX i] $
       i <== (true `and` false)
 
+testArrays :: Prog ()
+testArrays = do
+  i <- newEntity "arrays" $
+         newPort InOut true :: Prog (Signal Bool)
+  newArchitecture "arrays" "behavioural" $
+    newProcess [toX i] $
+      do a <- newArray (litE 4) :: Prog (Array Int8 Bool)
+         v <- getArray (litE 2) a
+         i <== v
+
 --------------------------------------------------------------------------------
 
 printTests :: IO ()
 printTests = do
+  putStrLn "\n### Simple ###"
   putStrLn $ compile $ testSimple
+  putStrLn "\n### Arrays ###"
+  putStrLn $ compile $ testArrays
 
 --------------------------------------------------------------------------------
