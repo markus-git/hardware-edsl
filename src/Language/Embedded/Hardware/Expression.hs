d833af4211899b75f64f45a02ec6ea54fbbe82b8
Markus <mararon@chalmers.se>
2016-03-15 02:10:26 +0100

    new representation of variable length bit arrays using type lits

--- src/Language/Embedded/Hardware/Expression.hs
+++ src/Language/Embedded/Hardware/Expression.hs
@@ -1,11 +1,14 @@
 module Language.Embedded.Hardware.Expression
   ( HExp
   , HType
+    
   , module Language.Embedded.Hardware.Expression.Frontend
   , module Language.Embedded.Hardware.Expression.Represent
+  , module Language.Embedded.Hardware.Expression.Represent.Bit
   ) where
 
 import Language.Embedded.Hardware.Expression.Syntax (HExp, HType)
 import Language.Embedded.Hardware.Expression.Frontend
 import Language.Embedded.Hardware.Expression.Represent
+import Language.Embedded.Hardware.Expression.Represent.Bit
 import Language.Embedded.Hardware.Expression.Backend.VHDL ()
