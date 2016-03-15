4779805e1faf4e819412deeb1be6b31cd47d17b6
Markus <mararon@chalmers.se>
2016-03-15 09:40:43 +0100

    renamed Bit->Bits and set Bit=Bool

--- src/Language/Embedded/Hardware/Expression/Represent/Bit.hs
+++ src/Language/Embedded/Hardware/Expression/Represent/Bit.hs
@@ -6,6 +6,7 @@
 
 module Language.Embedded.Hardware.Expression.Represent.Bit
   ( Bit
+  , Bits
   , bitFromInteger
   , bitToInteger
   , bitAdd
@@ -43,7 +44,8 @@ import Language.Embedded.VHDL            (VHDL)
 import Language.Embedded.VHDL.Monad      (newSym, newLibrary, newImport, constrainedArray)
 import Language.Embedded.VHDL.Monad.Type
 
-import Data.Bits       (Bits(..))
+import Data.Bits       hiding (Bits)
+import qualified Data.Bits as Bit (Bits)
 import Data.Ix
 import Control.Monad   (guard)
 import Control.DeepSeq (NFData(..))
@@ -56,16 +58,18 @@ import GHC.TypeLits
 -- * ...
 --------------------------------------------------------------------------------
 
-newtype Bit (n :: Nat) = B Integer
+type    Bit = Bool
 
-instance forall n. KnownNat n => Rep (Bit n)
+newtype Bits (n :: Nat) = B Integer
+
+instance forall n. KnownNat n => Rep (Bits n)
   where
-    declare = declareBit 
+    declare = declareBits
     format  = bitShowBin
 
-declareBit :: forall proxy n. KnownNat n => proxy (Bit n) -> VHDL Type
-declareBit _ = declareBoolean >> return (std_logic_vector size)
-  where size = fromInteger (ni (undefined :: Bit n))
+declareBits :: forall proxy n. KnownNat n => proxy (Bits n) -> VHDL Type
+declareBits _ = declareBoolean >> return (std_logic_vector size)
+  where size = fromInteger (ni (undefined :: Bits n))
         
 --------------------------------------------------------------------------------
 -- ** ...
@@ -73,138 +77,138 @@ declareBit _ = declareBoolean >> return (std_logic_vector size)
 ni :: KnownNat n => proxy n -> Integer
 ni = fromIntegral . natVal
 
-norm :: KnownNat n => Bit n -> Bit n
+norm :: KnownNat n => Bits n -> Bits n
 norm b@(B n) = B (n .&. ((1 `shiftL` fromInteger (ni b)) - 1))
 
-bitFromInteger :: KnownNat n => Integer -> Bit n
+bitFromInteger :: KnownNat n => Integer -> Bits n
 bitFromInteger i = norm (B i)
 
-bitToInteger :: Bit n -> Integer
+bitToInteger :: Bits n -> Integer
 bitToInteger (B i) = i
 
 --------------------------------------------------------------------------------
 -- ** ...
 
-lift1 :: KnownNat n => (Integer -> Integer) -> Bit n -> Bit n
+lift1 :: KnownNat n => (Integer -> Integer) -> Bits n -> Bits n
 lift1 f (B i) = norm (B (f i))
 
-lift2 :: KnownNat n => (Integer -> Integer -> Integer) -> Bit n -> Bit n -> Bit n
+lift2 :: KnownNat n => (Integer -> Integer -> Integer) -> Bits n -> Bits n -> Bits n
 lift2 f (B i) (B j) = norm (B (f i j))
 
 --------------------------------------------------------------------------------
 -- ** ...
 
-bitAdd :: KnownNat n => Bit n -> Bit n -> Bit n
+bitAdd :: KnownNat n => Bits n -> Bits n -> Bits n
 bitAdd = lift2 (+)
 
-bitSub :: KnownNat n => Bit n -> Bit n -> Bit n
+bitSub :: KnownNat n => Bits n -> Bits n -> Bits n
 bitSub = lift2 (-)
 
-bitMul :: KnownNat n => Bit n -> Bit n -> Bit n
+bitMul :: KnownNat n => Bits n -> Bits n -> Bits n
 bitMul = lift2 (*)
 
-bitAdd' :: Bit n -> Bit n -> Bit (n + 1)
+bitAdd' :: Bits n -> Bits n -> Bits (n + 1)
 bitAdd' (B i) (B j) = B (i + j)
 
-bitSub' :: Bit n -> Bit n -> Bit (n + 1)
+bitSub' :: Bits n -> Bits n -> Bits (n + 1)
 bitSub' (B i) (B j) = B (i - j)
 
-bitMul' :: Bit n -> Bit n -> Bit (n + n)
+bitMul' :: Bits n -> Bits n -> Bits (n + n)
 bitMul' (B i) (B j) = B (i * j)
 
-bitQuotRem :: Bit n -> Bit n -> (Bit n, Bit n)
+bitQuotRem :: Bits n -> Bits n -> (Bits n, Bits n)
 bitQuotRem (B i) (B j) = let (a, b) = quotRem i j in (B a, B b)
 
-bitNeg :: KnownNat n => Bit n -> Bit n
+bitNeg :: KnownNat n => Bits n -> Bits n
 bitNeg = lift1 negate
 
-bitMinBound :: Bit n
+bitMinBound :: Bits n
 bitMinBound = B 0
 
-bitMaxBound :: KnownNat n => Bit n
+bitMaxBound :: KnownNat n => Bits n
 bitMaxBound = norm (B (-1))
 
-bitSigNum :: Bit n -> Bit n
+bitSigNum :: Bits n -> Bits n
 bitSigNum (B i) = B (signum i)
 
-bitAnd :: Bit n -> Bit n -> Bit n
+bitAnd :: Bits n -> Bits n -> Bits n
 bitAnd (B i) (B j) = B (i .&. j)
 
-bitOr :: Bit n -> Bit n -> Bit n
+bitOr :: Bits n -> Bits n -> Bits n
 bitOr (B i) (B j) = B (i .|. j)
 
-bitXor :: Bit n -> Bit n -> Bit n
+bitXor :: Bits n -> Bits n -> Bits n
 bitXor (B i) (B j) = B (i .|. j)
 
-bitComplement :: KnownNat n => Bit n -> Bit n
+bitComplement :: KnownNat n => Bits n -> Bits n
 bitComplement = lift1 complement
 
-bitSplit :: (KnownNat m, KnownNat n) => proxy m -> Bit (m + n) -> (Bit m, Bit n)
+bitSplit :: (KnownNat m, KnownNat n) => proxy m -> Bits (m + n) -> (Bits m, Bits n)
 bitSplit m (B i) = (a, b)
   where a = B (i `shiftR` fromInteger (ni m))
         b = bitFromInteger i
 
-bitJoin :: KnownNat n => Bit m -> Bit n -> Bit (m + n)
+bitJoin :: KnownNat n => Bits m -> Bits n -> Bits (m + n)
 bitJoin (B i) b@(B j) = B (shiftL i (fromInteger (ni b)) .|. j)
 
-bitCoerce :: forall n m. (KnownNat n, KnownNat m) => Bit n -> Maybe (Bit m)
+bitCoerce :: forall n m. (KnownNat n, KnownNat m) => Bits n -> Maybe (Bits m)
 bitCoerce b@(B i) = guard (ni b == ni d) >> return (B i)
-  where d = undefined :: Bit m
+  where d = undefined :: Bits m
 
-bitShiftR :: Bit n -> Int -> Bit n
+bitShiftR :: Bits n -> Int -> Bits n
 bitShiftR (B i) n = B (shiftR i n)
 
-bitShiftL :: KnownNat n => Bit n -> Int -> Bit n
+bitShiftL :: KnownNat n => Bits n -> Int -> Bits n
 bitShiftL b n = lift1 (`shiftL` n) b
 
-bitTestBit :: Bit n -> Int -> Bool
+bitTestBit :: Bits n -> Int -> Bool
 bitTestBit (B i) n = testBit i n
 
-bitRotate :: KnownNat n => Bit n -> Int -> Bit n
+bitRotate :: KnownNat n => Bits n -> Int -> Bits n
 bitRotate b@(B i) n
   | si < 2    = b
   | otherwise = bitOr (bitFromInteger (shiftL i n)) (bitFromInteger (shiftR i (si - n)))
   where n' = mod n si
         si = fromInteger (ni b)
 
-bitToList :: KnownNat n => Bit n -> [Bool]
+bitToList :: KnownNat n => Bits n -> [Bool]
 bitToList b = map (bitTestBit b) [start, start - 1 .. 0]
   where start = fromInteger (ni b)
 
-bitReadsPrec :: KnownNat n => Int -> ReadS (Bit n)
+bitReadsPrec :: KnownNat n => Int -> ReadS (Bits n)
 bitReadsPrec p txt = [ (bitFromInteger b, cs) | (b, cs) <- readsPrec p txt ]
 
-bitShowBin :: KnownNat n => Bit n -> String
+bitShowBin :: KnownNat n => Bits n -> String
 bitShowBin = map sh . bitToList
   where sh x = if x then '1' else '0'
 
-bitShowHex :: KnownNat n => Bit n -> String
+bitShowHex :: KnownNat n => Bits n -> String
 bitShowHex b@(B i) = zeros (N.showHex i "")
   where zeros n = replicate (len - length n) '0' ++ n
         len     = div (fromInteger (ni b) + 3) 4
 
 --------------------------------------------------------------------------------
 
-instance Show (Bit n) where
+instance Show (Bits n) where
   showsPrec p (B x) = showsPrec p x
 
-instance KnownNat n => Read (Bit n) where
+instance KnownNat n => Read (Bits n) where
   readsPrec = bitReadsPrec
 
-instance Eq (Bit n) where
+instance Eq (Bits n) where
   B i == B j = i == j
 
-instance NFData (Bit n) where
+instance NFData (Bits n) where
   rnf (B i) = seq i ()
 
-instance Ord (Bit n) where
+instance Ord (Bits n) where
   compare (B i) (B j) = compare i j
 
-instance KnownNat n => Bounded (Bit n) where
+instance KnownNat n => Bounded (Bits n) where
   minBound = bitMinBound
   maxBound = bitMaxBound
 
-instance KnownNat n => Num (Bit n) where
+instance KnownNat n => Num (Bits n) where
   (+)           = bitAdd
   (-)           = bitSub
   (*)           = bitMul
@@ -213,7 +217,7 @@ instance KnownNat n => Num (Bit n) where
   signum        = bitSigNum
   fromInteger   = bitFromInteger
 
-instance KnownNat n => Bits (Bit n) where
+instance KnownNat n => Bit.Bits (Bits n) where
   isSigned _    = False
   bit           = bitFromInteger . (2 ^)
   bitSize       = fromInteger . ni
@@ -228,10 +232,10 @@ instance KnownNat n => Bits (Bit n) where
   rotate        = bitRotate
   popCount      = length . filter id . bitToList
 
-instance KnownNat n => Real (Bit n) where
+instance KnownNat n => Real (Bits n) where
   toRational (B i) = toRational i
 
-instance KnownNat n => Enum (Bit n) where
+instance KnownNat n => Enum (Bits n) where
   toEnum i        = norm $ B $ toEnum i
   fromEnum (B i)  = fromEnum i
   succ i          = i + 1
@@ -249,11 +253,11 @@ instance KnownNat n => Enum (Bit n) where
 
   enumFromThenTo (B i) (B j) (B k) = map B (enumFromThenTo i j k)
 
-instance KnownNat n => Integral (Bit n) where
+instance KnownNat n => Integral (Bits n) where
   toInteger = bitToInteger
   quotRem   = bitQuotRem
 
-instance KnownNat n => Ix (Bit n) where
+instance KnownNat n => Ix (Bits n) where
   range   = undefined
   index   = undefined
   inRange = undefined
